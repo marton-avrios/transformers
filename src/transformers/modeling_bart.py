4c4408a724a8bbb77a947f2dd8220328168944cb
Marton Gyorgy <marton.gyorgy@avrios.com>
2020-07-27 15:35:42 +0200

    tpu 2

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -951,6 +951,7 @@ class BartForConditionalGeneration(PretrainedBartModel):
         old_num_tokens = self.model.shared.num_embeddings
         new_embeddings = super().resize_token_embeddings(new_num_tokens)
         self.model.shared = new_embeddings
+        self.lm_head = _make_linear_from_emb(self.model.shared)
         self._resize_final_logits_bias(new_num_tokens, old_num_tokens)
         return new_embeddings
 
